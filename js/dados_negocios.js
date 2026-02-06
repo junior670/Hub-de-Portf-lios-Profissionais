95c8baec56fcfc6596b78c78d60cd720110a5e7d
Júnior670 (Júnior670) <85705837+junior670@users.noreply.github.com>
2026-02-05 22:28:00 -0300

    Delete js directory

--- js/dados_negocios.js
+++
@@ -1,8 +0,0 @@
-const listaNegocios = [
-    {
-        nome: "Inova Tech Solutions",
-        link: "#",
-        desc: "Consultoria local para digitalização de pequenas empresas.",
-        tags: "| Consultoria 🔥 | Jundiaí/SP 🔥"
-    }
-];
